6c5785e1ff8cd55d28f61e4f6e044b798e158296
mosecoza <mosecoza@gmail.com>
2019-10-21 08:01:42 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './components/homepage'
+import './components/homepage.scss';
+
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
